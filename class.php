ea739fe6a76eb261923e9432ecc3327a0f073fca
Mirko Guerra <mirkoguerra93@gmail.com>
2021-01-18 17:48:47 +0100

    Senza impiegato su commissione

---
+++ class.php
@@ -0,0 +1,99 @@
+<?php
+
+class Persona
+{
+
+  protected $nome;
+  protected $cognome;
+  protected $codice_fiscale;
+
+  public function __construct($name, $lastname, $f_c)
+  {
+    $this->nome = $name;
+    $this->cognome = $lastname;
+    $this->codice_fiscale = $f_c;
+  }
+
+  public function to_string()
+  {
+    echo 'Nome: '.$this->nome.'</br>Cognome: '.$this->cognome.'</br>Codice fiscale: '.$this->codice_fiscale;
+  }
+
+}
+
+class Impiegato extends Persona
+{
+
+  protected $codice_impiegato;
+  public $compenso;
+
+  public function __construct($name, $lastname, $f_c, $empl_code)
+  {
+    parent::__construct($name, $lastname, $f_c);
+    $this->codice_impiegato = $empl_code;
+  }
+
+  public function calcola_compenso()
+  {
+    return $this->compenso;
+  }
+
+  public function to_string()
+  {
+    echo 'Nome: '.$this->nome.'</br>Cognome: '.$this->cognome.'</br>Codice fiscale: '.$this->codice_fiscale.'</br>Codice impiegato: '.$this->codice_impiegato.'</br>Compenso: '.$this->calcola_compenso();
+  }
+
+}
+
+class ImpiegatoSalariato extends Impiegato
+{
+
+  private $giorni_lavorati;
+  private $compenso_giornaliero;
+
+  public function __construct($name, $lastname, $f_c, $empl_code, $work_days, $daily_retr)
+  {
+    parent::__construct($name, $lastname, $f_c, $empl_code);
+    $this->giorni_lavorati = $work_days;
+    $this->compenso_giornaliero = $daily_retr;
+  }
+
+  public function calcola_compenso()
+  {
+    return $this->giorni_lavorati*$this->compenso_giornaliero;
+  }
+
+}
+
+class ImpiegatoAOre extends Impiegato
+{
+
+  private $ore_lavorate;
+  private $compenso_orario;
+
+  public function __construct($name, $lastname, $f_c, $empl_code, $work_h, $h_retr)
+  {
+    parent::__construct($name, $lastname, $f_c, $empl_code);
+    $this->ore_lavorate = $work_h;
+    $this->compenso_orario = $h_retr;
+  }
+
+  public function calcola_compenso()
+  {
+    return $this->ore_lavorate*$this->compenso_orario;
+  }
+
+}
+
+class ImpiegatoSuCommissione extends Impiegato
+{
+
+  function __construct($name, $lastname, $f_c, $empl_code)
+  {
+    parent::__construct($name, $lastname, $f_c, $empl_code);
+  }
+
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <title>php-oop-dipendenti</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- PER LA VISUALIZZAZIONE -->
+
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <!-- PER IL MIO CSS -->
+
+  </head>
+  <body>
+
+    <?php
+
+    require_once('class.php');
+
+    $persona_1 = new Persona('Mirko', 'Guerra', 'GRRMRK93R30C758T');
+
+    $impiegato_1 = new Impiegato('Mirko', 'Guerra', 'GRRMRK93R30C758T', 0001);
+    $impiegato_1->compenso = 1000;
+
+    $impiegato_salariato_1 = new ImpiegatoSalariato('Mirko', 'Guerra', 'GRRMRK93R30C758T', 0001, 10, 100);
+
+    $impiegato_a_ore_1 = new ImpiegatoAOre('Matteo', 'Bianchi', 'AAAABBBBCCCCDDDD', 0027, 400, 7);
+
+    ?>
+    <h1>Persona:</h1>
+    <p><?php echo $persona_1->to_string(); ?></p>
+
+    <h1>Impiegato:</h1>
+    <p><?php echo $impiegato_1->to_string(); ?></p>
+
+    <h1>Impiegato salariato:</h1>
+    <p><?php echo $impiegato_salariato_1->to_string(); ?></p>
+
+    <h1>Impiegato a ore:</h1>
+    <p><?php echo $impiegato_a_ore_1->to_string(); ?></p>
+
+    <h1>Impiegato su commissione:</h1>
+
+  </body>
+</html>
